5d91a3981466a2af38175a756404f90fff2b8fbf
Devin Miller <devzmiller@gmail.com>
2017-10-14 13:42:27 -0700

    Add show and new routes and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    params.permit!
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      @errors = @post.errors.full_messages
+      render 'new'
+    end
+  end
 end
