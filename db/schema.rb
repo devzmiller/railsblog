f5c0f8906ad9c27c19de26653ad9271b575330e4
Devin Miller <devzmiller@gmail.com>
2017-10-14 19:57:00 -0700

    Add bcrypt gem. Create user model/migrations and pass tests for password hashing and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014195021) do
+ActiveRecord::Schema.define(version: 20171015024722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20171014195021) do
     t.string "title"
     t.text "body", null: false
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "hashed_password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
